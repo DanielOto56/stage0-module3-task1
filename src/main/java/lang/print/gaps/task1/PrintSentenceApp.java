3aae6ee8c4437b4e604cd6149939878a85d5ba6c
Daniel Oto <daniel.oto.mail@gmail.com>
2022-07-13 23:00:19 +0300

    task 2, filled/fixed codes

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,11 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.print("This ");
-        System.out.print("is ");
-        System.out.print("my ");
-        System.out.print("first ");
-        System.out.print("Java ");
-        System.out.print("program");
+        System.out.println("This is my first Java program");
     }
 }
